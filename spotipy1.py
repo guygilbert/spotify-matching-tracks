db6e272f03ff8d9f2df788df0fa757fddddaa1a2
Guy Gilbert <guygilbert@hotmail.com>
2016-09-13 02:01:14 -0700

    Worked on python

--- spotipy1.py
+++ spotipy1.py
@@ -1,19 +1,31 @@
 import spotipy
+import spotipy.util as util
 
 #Here is the flow of the app: 
+#0. Authenticate with Spotify credentials
 #1. Find track playing now
 #2. Find tracks with similar genre, key, BPM
-#3. Find tracks that match some of the criteria
+#3. Find tracks that match some of the criteria (P2)
 #4. Offer choice and queue the chosen track
 
-fourtet_uri = 'spotify:artist:7Eu1txygG6nJttLHbZdQOh'
-spotify = spotipy.Spotify()
+username = 'guygilbert66'
+scope = 'user-library-read'
+client_id = '261fdbf599a4419291e83c05ddadf224'
+client_secret = '6e1bc3b96d9842c4a445039882879606'
+redirect_uri = 'http://192.168.1.31/'
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+token = util.prompt_for_user_token(username,scope,client_id,client_secret,redirect_uri)
 
-for album in albums:
-    print(album['name'])
+if token:
+	sp = spotipy.Spotify(auth=token)
+	results = sp.current_user_saved_tracks(2)
+	for item in results['items']:
+		track = item['track']
+		print(track['name'] + ' - ' + track['artists'][0]['name'])
+	recos = sp.recommendations(seed_genres=['deep-house','chill'],limit=3,country='US')
+	for item in recos['tracks']:
+		track = item['track_number']
+		print(track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print("Can't get token for", username)
+
